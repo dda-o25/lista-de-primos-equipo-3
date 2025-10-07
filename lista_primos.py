d42cefde7c2180c6d8f221043de3547643e39ff4
EricPanas <eric.panas@iteso.mx>
2025-10-07 16:01:06 -0600

    primer intento

--- lista_primos.py
+++ lista_primos.py
@@ -1,15 +1,42 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Enrique Aranda Gutiérrez
+Carlos Ariel González Ramírez
+Eric Fernando Panas López Dellamary
+Juan Pablo Zepeda Orozco
+
+07/10/2025
+
+Dado un número, el programa regresa todos los números primos 
+menores o iguales a dicho número.
 """
 
-# Declaraciones
-CONSTANTE = valor
+#Declaraciones
+def es_primo(numero):
+    if numero > 1:
+        primo = True
+        x = 2
+        while x < numero:
+            if numero % x == 0:
+                primo = False
+                break
+            x += 1
+    return primo
 
 # Entradas
-entrada = input()
+num = int(input("Ingrese un número entero: "))
 
 # Proceso
+indice = 0
+primos = []
 
+for numero in range(2,num+1):
+    if es_primo(numero):
+        primos.append(numero)
+
+primos_salida = str(primos)[1:-1]
 
 # Salidas
-print(salida)
+print(f'Los números primos menores o iguales a {num} son: {primos_salida}.')
+
+
+#Los números primos menores o iguales a 30 son: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29.
